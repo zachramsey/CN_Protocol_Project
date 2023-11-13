33642ab9810d795ad3e81a30a2b49f82f3e03fa3
EmSheshan <emsheshan@gmail.com>
2023-11-13 15:08:50 -0600

    added basic client-server code with three-way handshake
    Signed-off-by: EmSheshan <emsheshan@gmail.com>

--- client.py
+++ client.py
@@ -1,1 +1,34 @@
+import socket
 
+
+def application_layer_handshake(client_socket):
+    # Send a "HELLO" message to the server
+    hello_message = "HELLO: Establish connection request."
+    client_socket.sendall(hello_message.encode('utf-8'))
+    print("Sent HELLO message to server")
+
+    # Receive the acknowledgment ("ACK") message from the server
+    ack_message = client_socket.recv(1024).decode('utf-8')
+    print("Received ACK message from server:", ack_message)
+
+
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+server_address = ('localhost', 12345)
+client_socket.connect(server_address)
+
+# Perform the application layer handshake
+application_layer_handshake(client_socket)
+
+# Send a message to the server
+message = "Hello, server! This is a message."
+client_socket.sendall(message.encode('utf-8'))
+
+# Receive and print the confirmation from the server
+confirmation = client_socket.recv(1024)
+print("Server confirmation:", confirmation.decode('utf-8'))
+
+# Close the connection
+client_socket.close()
--- server.py
+++ server.py
@@ -1,1 +1,41 @@
+import socket
 
+
+def application_layer_handshake(client_socket):
+    # Receive the client's "HELLO" message
+    hello_message = client_socket.recv(1024).decode('utf-8')
+    print("Received HELLO message from client:", hello_message)
+
+    # Send an acknowledgment ("ACK") message back to the client
+    ack_message = "ACK: Connection established."
+    client_socket.sendall(ack_message.encode('utf-8'))
+    print("Sent ACK message to client")
+
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to a specific address and port
+server_address = ('localhost', 12345)
+server_socket.bind(server_address)
+
+# Listen for incoming connections
+server_socket.listen(5)
+
+while True:
+    # Wait for a connection
+    client_socket, client_address = server_socket.accept()
+
+    # Perform the application layer handshake
+    application_layer_handshake(client_socket)
+
+    # Handle the connection (e.g., receive and process messages)
+    data = client_socket.recv(1024)
+    print("Received message from client:", data.decode('utf-8'))
+
+    # Send a confirmation back to the client
+    confirmation_message = "Message received by the server."
+    client_socket.sendall(confirmation_message.encode('utf-8'))
+
+    # Close the connection
+    client_socket.close()
