9d72862f2f12d0d6791c723f01cecba5b4533141
EmSheshan <emsheshan@gmail.com>
2023-11-17 10:00:54 -0600

    Finished up the code: fleshed out the handshake and added error checking Signed-off-by: EmSheshan <emsheshan@gmail.com>

--- client.py
+++ client.py
@@ -1,15 +1,44 @@
 import socket
+import struct
 
 
-def application_layer_handshake(client_socket):
-    # Send a "HELLO" message to the server
+def send_data(client_socket, data):
+    message = struct.pack('!4sHH', b'DATA', len(data), calculate_checksum(data)) + data.encode('utf-8')
+    client_socket.sendall(message)
+
+
+def send_ack(client_socket, acknowledgment):
+    message = struct.pack('!4sHH', b'ACK', len(acknowledgment),
+                          calculate_checksum(acknowledgment)) + acknowledgment.encode('utf-8')
+    client_socket.sendall(message)
+
+
+def send_hello(client_socket):
     hello_message = "HELLO: Establish connection request."
-    client_socket.sendall(hello_message.encode('utf-8'))
-    print("Sent HELLO message to server")
+    message = struct.pack('!4sHH', b'HELLO', len(hello_message),
+                          calculate_checksum(hello_message)) + hello_message.encode('utf-8')
+    client_socket.sendall(message)
 
-    # Receive the acknowledgment ("ACK") message from the server
-    ack_message = client_socket.recv(1024).decode('utf-8')
-    print("Received ACK message from server:", ack_message)
+
+def receive_hello(client_socket):
+    # Receive the handshake message header
+    header = client_socket.recv(8)
+    message_type, length, checksum = struct.unpack('!4sHH', header)
+
+    # Receive the handshake message content based on the length
+    hello_message = client_socket.recv(length).decode('utf-8')
+
+    # Verify the checksum
+    if checksum == calculate_checksum(hello_message):
+        return hello_message
+    else:
+        return None  # Handle checksum verification failure
+
+
+def calculate_checksum(data):
+    # Calculate the checksum by summing ASCII values of characters
+    checksum = sum(ord(char) for char in data)
+    return checksum & 0xFFFF  # Keep it within a 16-bit range
 
 
 # Create a socket object
@@ -19,16 +48,29 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 12345)
 client_socket.connect(server_address)
 
-# Perform the application layer handshake
-application_layer_handshake(client_socket)
+handshake = False
+while not handshake:
+    # Perform the application layer handshake
+    send_hello(client_socket)
 
-# Send a message to the server
-message = "Hello, server! This is a message."
-client_socket.sendall(message.encode('utf-8'))
+    # Receive the acknowledgment ("ACK") message from the server
+    ack_message = receive_hello(client_socket)
+
+    if ack_message:
+        print("Received ACK message from server:", ack_message)
+        print("Connection established.")
+        handshake = True
+    else:
+        print("Failed to establish connection.")
+
+
+# Send a message to server
+message_to_server = input("Enter your message: ")
+send_data(client_socket, message_to_server)
 
 # Receive and print the confirmation from the server
-confirmation = client_socket.recv(1024)
-print("Server confirmation:", confirmation.decode('utf-8'))
+confirmation_message = client_socket.recv(1024).decode('utf-8')
+print("Server confirmation:", confirmation_message)
 
 # Close the connection
 client_socket.close()
